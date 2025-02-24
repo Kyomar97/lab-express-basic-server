16ce6d01e0245c4b1b7ca56ff64ee0fb1a2a75ee
Sebna Omar <omargr97@gmail.com>
2025-02-24 12:46:20 +0000

    done lab-express-basic-server

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
-
-
+const projects = require("./data/projects.json");
+const articles = require("./data/articles.json");
+const express = require(`express`);
+const morgan = require(`morgan`);
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
 
-
+const app = express();
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
@@ -14,12 +16,30 @@
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
 
-
+app.use(express.static(`public`));
+app.use(express.json());
+app.use(morgan(`dev`));
 
 // ROUTES
 // Start defining your routes here:
-
-
+app.get(`/`, (resq, resp) => {
+  resp.sendFile(__dirname + "/views/home.html");
+});
+app.get(`/blog`, (resq, resp) => {
+  resp.sendFile(__dirname + "/views/blog.html");
+});
+app.get(`/api/projects`, (resq, resp) => {
+  resp.json(projects);
+});
+app.get(`/api/articles`, (resq, resp) => {
+  resp.json(articles);
+});
+app.use((resq, resp, next) => {
+  resp.status(404).sendFile(__dirname + "/views/not-found.html");
+});
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, () => {
+  console.log("El servidor esta encendido");
+});
